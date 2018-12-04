f17b101cde77edf63c0538620f2b57fea5ae148f
Diego Zamboni <diego@zzamboni.org>
2018-12-04 22:25:49 +0100

    Renamed solution and renamed incomplete file

--- 2018/04/solution1.rkt
+++
@@ -1,7 +0,0 @@
-#lang rash
-(require rash/demo/setup)
-
-(define input #{cat input.txt |> port->lines |> sort _ string<? })
-
-(for/list ([l input])
-  (regexp-match #rx"\\[\\d\\d\\d\\d-\\d\\d-\\d\\d \\d\\d:(\\d\\d)\\] Guard #(\\d+) begins shift" l))
