189120d28fa85753187debe80bc011fd861988e7
Diego Zamboni <diego@zzamboni.org>
2018-12-12 07:57:49 +0100

    Some cleanup

--- 2018/08/solution.rkt
+++ 2018/08/solution.rkt
@@ -2,17 +2,15 @@
 (define tree-numbers (map string->number (string-split (file->string "input.txt"))))
 
 (define (extract-tree tlist pos)
-  ;;(printf "Expanding ~a starting at ~a~n" tlist pos)
   (let* ([tsublist (drop tlist pos)]
          [n-subtrees (first tsublist)]
          [n-metadata (second tsublist)])
     (let-values ([(subtrees lastpos)
-                  (for/fold ([-subtrees '()]
-                             [-lastpos 2])
+                  (for/fold ([_subtrees '()]
+                             [_lastpos 2])
                             ([n (in-range n-subtrees)])
-                    (let-values ([(tree endpos) (extract-tree tsublist -lastpos)])
-                      (values (append -subtrees (list tree)) (+ -lastpos endpos)))
-                    )])
+                    (let-values ([(tree endpos) (extract-tree tsublist _lastpos)])
+                      (values (append _subtrees (list tree)) (+ _lastpos endpos))))])
       (let* ([meta (take (drop tsublist lastpos) n-metadata)]
              [treestruct (list n-subtrees n-metadata subtrees meta)])
         (values treestruct (+ lastpos n-metadata))))))
@@ -21,14 +19,16 @@
   (let-values ([(tstruct n) (extract-tree tlist 0)])
     tstruct))
 
-(define (traverse-apply tree func init)
-  (match-let ([(list a b c d) tree])
-    (for/fold ([new (func init tree)])
+(define (traverse-apply node func init)
+  (match-let ([(list a b c d) node])
+    (for/fold ([new (func init node)])
               ([subs c])
-      ;;(printf "tree=~a new=~a subs=~a~n" tree new subs)
       (traverse-apply subs func new))))
 
-(define (first-result) (traverse-apply (parse-tree tree-numbers) (lambda (v t) (apply + v (fourth t))) 0))
+(define (first-result)
+  (traverse-apply (parse-tree tree-numbers)
+                  (lambda (v t) (apply + v (fourth t)))
+                  0))
 
 (define (node-value t)
   (match-let ([(list a b c d) t])
