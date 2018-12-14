e35aef09d39570f5f3befd8a6f8a5e1eb9607b27
Diego Zamboni <diego@zzamboni.org>
2018-12-14 22:42:58 +0100

    Code restructure and cleanup
    Restructured to be able to call the game with arbitrary parameters:
    
    (game n-players max-marble)

--- 2018/09/solution.rkt
+++ 2018/09/solution.rkt
@@ -1,15 +1,6 @@
 #lang racket
 (require racket/match)
 
-(match-define (list n-players last-marble)
-  (map string->number
-       (cdr (regexp-match #px"(\\d+) players; last marble is worth (\\d+) points"
-                          (file->string "input.txt")))))
-
-(define ring (make-hash '((marbles . (0)) (current . 0))))
-
-(define player-scores (make-vector n-players 0))
-
 (define (ring-pos ring delta)
   (modulo (+ (hash-ref ring 'current) delta) (length (hash-ref ring 'marbles))))
                             
@@ -21,6 +12,9 @@
 
 (define (ring-marbles ring)
   (hash-ref ring 'marbles))
+
+(define (ring-scores ring)
+  (hash-ref ring 'scores))
 
 ; Add v in the position after pos
 (define (ring-add! ring pos v)
@@ -44,21 +38,39 @@
                                          [i (in-naturals)])
                                 (if (= i (ring-current ring)) (format "(~a)" m) (format "~a" m))) " ")))
 
-(define (play player marble)
+(define (play ring player marble [trace-player -1])
   (if (zero? (modulo marble 23))
       (let* ([dropped-pos (ring-pos ring -7)]
-             [dropped-marble (ring-drop! ring dropped-pos)])
-        (vector-set! player-scores player (+ (vector-ref player-scores player) marble dropped-marble))
+             [dropped-marble (ring-drop! ring dropped-pos)]
+             [old-score (vector-ref (ring-scores ring) player)]
+             [new-score (+ old-score marble dropped-marble)])
+;        (when (or (= player trace-player) (= trace-player -2))
+;          ;(printf "Score for player #~a. Old score: ~a    Added marble: ~a (23*~a)    Dropped marble: ~a    New score: ~a~n" player old-score marble (/ marble 23) dropped-marble new-score)
+;          (printf "Dropped marble: ~a~n" dropped-marble)
+;          )
+        (vector-set! (ring-scores ring) player new-score)
+;        (vector-set! (hash-ref ring 'score-traces) player (append (vector-ref (hash-ref ring 'score-traces) player) (list marble)))
+;        (vector-set! (hash-ref ring 'dropped-marbles) player (append (vector-ref (hash-ref ring 'dropped-marbles) player) (list dropped-marble)))
         (ring-set-current! ring dropped-pos))
       (let ([newpos (ring-pos ring 1)])
         (ring-add! ring newpos marble)
         (ring-set-current! ring (add1 newpos)))))
 
-(define (play-all)
-  (for ([marble (in-range 1 (add1 last-marble))]
-        [i (in-naturals)])
-    (let ([player (modulo i n-players)])
-      (play player marble)
-      ;(ring-print ring)
-      ))
-  (printf "Max score: ~a~n" (apply max (vector->list player-scores))))
+(define (game n-players last-marble [trace-player -1])
+  (let ([ring (make-hash (list (cons 'marbles '(0))
+                               (cons 'current 0)
+                               (cons 'scores (make-vector n-players 0))
+                               (cons 'score-traces (make-vector n-players '()))
+                               (cons 'dropped-marbles (make-vector n-players '()))))])
+    (for ([marble (in-range 1 (add1 last-marble))]
+          [i (in-naturals)])
+      (let ([player (modulo i n-players)])
+        (play ring player marble trace-player)
+        ;(ring-print ring)
+        ))
+;    (printf "Score traces:~n~a~n" (string-join (map (lambda (v) (format "~a" v)) (vector->list (hash-ref ring 'score-traces))) "\n"))
+;    (printf "Dropped marbles:~n~a~n" (string-join (map (lambda (v) (format "~a" v)) (vector->list (hash-ref ring 'dropped-marbles))) "\n"))
+    (printf "Max score for ~a players and ~a marbles: ~a~n" n-players last-marble (apply max (vector->list (ring-scores ring))))))
+
+(require profile)
+(profile-thunk (lambda () (game 416 71617)))
